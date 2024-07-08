2e210d018f6732fc6f290fc6249fc5fbb731864c
Parthiba-Hazra <parthibahazra@gmail.com>
2024-07-08 23:46:30 +0530

    ci: Add CI workflow for checkpoint garbage collection
    Implemented a GitHub Actions workflow to test the checkpoint
    restore operator's ability to correctly delete checkpoints
    through garbage collection.
    
    - Added `wait_for_checkpoint_reduction.sh` script to check the
      number of checkpoint tar files and ensure it is reduced to
      target value within a 60-second timeout.
    - Added `generate_checkpoint_tar.sh` script to correctly
      create and move checkpoint tar files.
    - Added `run_tests.bats` to test different garbage
      collection policies of the operator
    
    Signed-off-by: Parthiba-Hazra <parthibahazra@gmail.com>

---
+++ test/generate_checkpoint_tar.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+DATA_DIR="./test/data"
+if [ ! -d "$DATA_DIR" ]; then
+	echo "Data directory '$DATA_DIR' does not exist."
+	exit 1
+fi
+
+CONFIG_DUMP="$DATA_DIR/config.dump"
+SPEC_DUMP="$DATA_DIR/spec.dump"
+if [ ! -f "$CONFIG_DUMP" ] || [ ! -f "$SPEC_DUMP" ]; then
+	echo "config.dump and/or spec.dump files are missing in the data directory."
+	exit 1
+fi
+
+TEMP_DIR=$(mktemp -d)
+
+cp "$CONFIG_DUMP" "$TEMP_DIR"
+cp "$SPEC_DUMP" "$TEMP_DIR"
+
+create_checkpoint_tar() {
+	local tar_name=$1
+	local original_name=$2
+	tar -cf "$tar_name" -C "$TEMP_DIR" .
+	sudo mv "$tar_name" "/var/lib/kubelet/checkpoints/$original_name"
+	echo "Checkpoint tar file created at /var/lib/kubelet/checkpoints/$original_name"
+}
+
+increment_timestamp() {
+	date -d "$1 + 1 second" +%Y-%m-%dT%H:%M:%S
+}
+
+TIMESTAMP=$(date +%Y-%m-%dT%H:%M:%S)
+
+for _ in {1..5}; do
+	TAR_NAME="checkpoint.tar"
+	ORIGINAL_NAME="checkpoint-podname_namespace-containername-$TIMESTAMP.tar"
+	create_checkpoint_tar "$TAR_NAME" "$ORIGINAL_NAME"
+	TIMESTAMP=$(increment_timestamp "$TIMESTAMP")
+done
+
+rm -rf "$TEMP_DIR"
